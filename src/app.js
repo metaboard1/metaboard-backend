a4584a07e1a2f9224e541ba0cc3c8a792a968edc
Yash Kumar Jha <yashkumarjha20@gmail.com>
2025-07-26 19:53:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,15 +11,6 @@ app.use(cors({
         "https://metaboard-admin-panel.vercel.app",
         "https://metaboard.in",
         "https://api.metaboard.in"
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-        'Origin',
-        'X-Requested-With',
-        'Content-Type',
-        'Accept',
-        'Authorization'
     ]
 }))
 
