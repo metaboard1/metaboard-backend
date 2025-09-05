f6af2fb14bd417bec2844f6fbb6689ad69590cf3
Yash Kumar Jha <100863383+Yash-dott@users.noreply.github.com>
2025-09-05 16:32:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 const app = require('./app')
 const {error} = require('./helpers/response')
 const requireDir = require('require-dir');
+const {S3Client} = require("@aws-sdk/client-s3");
 require('dotenv').config();
-const {v2: cloudinary} = require('cloudinary');
 
 requireDir('./routes/admin');
 requireDir('./routes/web');
-// requireDir('./routes');
 require('../src/models');
-const {S3Client} = require("@aws-sdk/client-s3");
 
 
 app.use((err, req, res, next) => {
@@ -22,19 +20,12 @@ app.use((err, req, res, next) => {
 
 const server = app.listen(process.env.PORT, async () => {
     console.log(`app is listening on port: ${process.env.PORT} `);
-
     global.s3Client = new S3Client({
         region: process.env.AWS_REGION,
         credentials: {
             accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
             secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY_ID
         }
-    });
-
-    cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_CLOUD_API_KEY,
-        api_secret: process.env.CLOUDINARY_CLOUD_SECRET_KEY
     });
 });
 
